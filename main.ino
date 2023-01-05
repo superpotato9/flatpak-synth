cc5b756916eb5331e15f66e27ad7d78cf9b4332f
nathan <50345854+superpotato9@users.noreply.github.com>
2023-01-04 21:22:34 -0800

    made adsr better got rid of sustain because decay handles it

--- main.ino
+++ main.ino
@@ -2,7 +2,7 @@
 /*
 2022
 code by:
-nathan Koliha 
+nathan Koliha,
 internalregister
 
 released under mit 
@@ -50,16 +50,16 @@ there are 3 main input blocks:
 
 */
 
-// each key is +1 half step above the last and each from c0 to c8 are stored here as bools theirfor notes can be found by doing octave * 12 + steps above 
-//array of notes spanning from c0 to b8 
- double key_values[] = {  16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87, 32.7, 34.65, 36.71, 38.89, 41.2, 43.65, 
- 46.25, 49.0, 51.91, 55.0, 58.27, 61.74, 65.41, 69.3, 73.42, 77.78, 82.41, 87.31, 92.5, 98.0, 103.83, 110.0, 116.54, 123.47, 130.81, 138.59, 146.83, 
- 155.56, 164.81, 174.61, 185.0, 196.0, 207.65, 220.0, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.0, 415.3, 440.0, 466.16, 
- 493.88, 523.25, 554.37, 587.33, 622.25, 659.25, 698.46, 739.99, 783.99, 830.61, 880.0, 932.33, 987.77, 1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91,
-  1479.98, 1567.98, 1661.22, 1760.0, 1864.66, 1975.53, 2093.0, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520.0, 3729.31, 3951.07,
-   4186.01, 4434.92, 4698.63, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040.0, 7458.62, 7902.13};
+// each key is +1 half step above the last and each from c0 to c8 are stored here as bools theirfor notes can be found by doing octave * 12 + steps above
+//array of notes spanning from c0 to b8
+double key_values[] = { 16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87, 32.7, 34.65, 36.71, 38.89, 41.2, 43.65,
+                        46.25, 49.0, 51.91, 55.0, 58.27, 61.74, 65.41, 69.3, 73.42, 77.78, 82.41, 87.31, 92.5, 98.0, 103.83, 110.0, 116.54, 123.47, 130.81, 138.59, 146.83,
+                        155.56, 164.81, 174.61, 185.0, 196.0, 207.65, 220.0, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.0, 415.3, 440.0, 466.16,
+                        493.88, 523.25, 554.37, 587.33, 622.25, 659.25, 698.46, 739.99, 783.99, 830.61, 880.0, 932.33, 987.77, 1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91,
+                        1479.98, 1567.98, 1661.22, 1760.0, 1864.66, 1975.53, 2093.0, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520.0, 3729.31, 3951.07,
+                        4186.01, 4434.92, 4698.63, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040.0, 7458.62, 7902.13 };
 
- 
+
 
 
 
@@ -74,59 +74,50 @@ it is worth noting that the tp, period, fine, and coarse functions are all used 
 
 
 
-double tp(double freq){// turns freq into tone period 
-return 2000000/(16*freq);
-
-
+double tp(double freq) {  // turns freq into tone period
+  return 2000000 / (16 * freq);
 }
-int env_period(double freq){ //envlope period
-  return (int)2000000/(256*freq);
-
+int env_period(double freq) {  //envlope period
+  return (int)2000000 / (256 * freq);
 }
-int coarse(double freq){ //calculates coarse tone value 
+int coarse(double freq) {  //calculates coarse tone value
   return (int)tp(freq) / 256;
 }
 
-int fine(double freq){ // calculates  fine tone value 
-return (int)tp(freq) - (256 - coarse(freq));
-
+int fine(double freq) {  // calculates  fine tone value
+  return (int)tp(freq) - (256 - coarse(freq));
 }
 
 
 
-int env_coarse(double freq){ //calculates coarse tone value 
+int env_coarse(double freq) {  //calculates coarse tone value
   return (int)env_period(freq) / 256;
 }
 
-int env_fine(double freq){ // calculates  fine tone value 
-return env_period(freq) - (256 -env_coarse(freq));
-
+int env_fine(double freq) {  // calculates  fine tone value
+  return env_period(freq) - (256 - env_coarse(freq));
 }
-void set_mode_inactive()
-{
+void set_mode_inactive() {
   digitalWrite(BC1_PIN, LOW);
   digitalWrite(BDIR_PIN, LOW);
 }
 
-void set_mode_latch()
-{
+void set_mode_latch() {
   digitalWrite(BC1_PIN, HIGH);
   digitalWrite(BDIR_PIN, HIGH);
 }
 
-void set_mode_write()
-{
+void set_mode_write() {
   digitalWrite(BC1_PIN, LOW);
   digitalWrite(BDIR_PIN, HIGH);
 }
 
-void write_register(char reg, char value)
-{
-  set_mode_latch();  
-  PORTD = reg;  
-  set_mode_inactive();  
-  set_mode_write();  
-  PORTD = value;  
+void write_register(char reg, char value) {
+  set_mode_latch();
+  PORTD = reg;
+  set_mode_inactive();
+  set_mode_write();
+  PORTD = value;
   set_mode_inactive();
 }
 /*
@@ -145,38 +136,36 @@ and for envlope and note on channel a:
 */
 
 
-void play_env(double freq){
+void play_env(double freq) {
 
- write_register(11, env_fine(freq));
-write_register(12, env_coarse(freq));  
-
-
+  write_register(11, env_fine(freq));
+  write_register(12, env_coarse(freq));
 }
 
 
 
-void play_a(double freq){ // plays a given note on channel a 
+void play_a(double freq) {  // plays a given note on channel a
 
- write_register(0, fine(freq));
-write_register(1, coarse(freq));  
+  write_register(0, fine(freq));
+  write_register(1, coarse(freq));
 }
-void stop_a(){
+void stop_a() {
   write_register(8, 0b00000000);
 }
-void play_b(int freq){ // plays a given note on channel b
+void play_b(int freq) {  // plays a given note on channel b
 
- write_register(2, fine(freq));
-write_register(3, coarse(freq));  
+  write_register(2, fine(freq));
+  write_register(3, coarse(freq));
 }
-void stop_b(){
+void stop_b() {
   write_register(9, 0b00000000);
 }
-void play_c(int freq){//plays given note on channel b 
-  
- write_register(4, fine(freq));
-write_register(5, coarse(freq)); 
+void play_c(int freq) {  //plays given note on channel b
+
+  write_register(4, fine(freq));
+  write_register(5, coarse(freq));
 }
-void stop_c(){
+void stop_c() {
   write_register(10, 0b00000000);
 }
 
@@ -184,17 +173,14 @@ void stop_c(){
 the drum function is peculear it plays random noise based on a given freq on channel c only initally it was only meant for drum sounds however it gives interesting sounds 
 when mixed with normal waveforms 
 */
-void play_drum(int freq){ 
+void play_drum(int freq) {
 
   write_register(7, 0b0100000);
-    write_register(6, fine(freq));
-
- 
-  
+  write_register(6, fine(freq));
 }
 
-void stop_drum(){
-   write_register(7, 0b0110000);
+void stop_drum() {
+  write_register(7, 0b0110000);
 }
 
 
@@ -214,54 +200,49 @@ wave chart:
 can be set via something: like wave_type(6);
 
 */
-void wave_type(int type )// takes an int as a representation of different waves  below is chart for them controls the envlope!
+void wave_type(int type)  // takes an int as a representation of different waves  below is chart for them controls the envlope!
 {
- if(type == 0){
+  if (type == 0) {
     write_register(8, 0b00001111);
     write_register(9, 0b00001111);
     write_register(10, 0b00001111);
 
-  }
-  else{
-   write_register(8, 0b00011111);
+  } else {
+    write_register(8, 0b00011111);
     write_register(9, 0b00011111);
     write_register(10, 0b00011111);
-
   }
-switch(type)
-{
- 
-  case 1: //this is used to pick the wave based on the number sent 
-write_register(13,0b00); //env control
-  
-  break;
-  case 2:
-write_register(13,0b01); //env control
-break;  
- case 3:
-write_register(13,0b1000); //env control
-break;  
- case 4:
-write_register(13,0b1010); //env control
-break;  
- case 5:
-write_register(13,0b1011); //env control
-break;  
- case 6:
-write_register(13,0b1100); //env control
-break;  
+  switch (type) {
 
- case 7:
-write_register(13,0b1101); //env control
-break;  
- case 8:
-write_register(13,0b1110); //env control
-break;  
-  default:
-  break;
-}
+    case 1:                      //this is used to pick the wave based on the number sent
+      write_register(13, 0b00);  //env control
 
+      break;
+    case 2:
+      write_register(13, 0b01);  //env control
+      break;
+    case 3:
+      write_register(13, 0b1000);  //env control
+      break;
+    case 4:
+      write_register(13, 0b1010);  //env control
+      break;
+    case 5:
+      write_register(13, 0b1011);  //env control
+      break;
+    case 6:
+      write_register(13, 0b1100);  //env control
+      break;
 
+    case 7:
+      write_register(13, 0b1101);  //env control
+      break;
+    case 8:
+      write_register(13, 0b1110);  //env control
+      break;
+    default:
+      break;
+  }
 }
 
 
@@ -269,11 +250,11 @@ break;
 
 
 /* 
-this block of wonderous code is the adsr functions for controlling amplitude they take 2-3 params that are used to generate their amplitude change 
-the level keyword sets the maximum level of the the function, the steps controls how long each step is normally the size of a step would be controlled but since integers cannot be used here
+this block of wonderous code is the adr functions for controlling amplitude 
+the steps controls how long each step is normally the size of a step would be controlled but since integers cannot be used here
 instead the length of each is used
 
-and finally the level of the previous function is needed to set the the starting point this is not present on the attack because it is the start of the entire adsr 
+and finally the level of the previous function is needed to set the the starting point this is not present on the attack because it is the start of the entire adr 
 the release function ends once the value of it reaches 0 or its min value
 * the steps is in ms 
 * all values are 4 bit ints and thus have a cap of 15
@@ -286,90 +267,66 @@ basic use is:
   play_a(440);
 
 
-  attack(14, 30);
-  decay(7, 100, 14);
-  sustain(9, 500, 7);
+  attack(30);
+  decay(14);
+
   release(0, 500, 9);
 
 
 
 note that this will loop unless properly stopped
 
-
+you might also note that there is not sustain function this is because decay takes care of it 
 */
 
 
-void adsr_mode(){// this is not strictly nessecary but very useful bc it disabled envlope on all channels
-    write_register(8, 0b00000000);
-    write_register(9, 0b00000000);
-    write_register(10, 0b00000000);
-
-
-
-
-
+void adsr_mode() {  // this is not strictly nessecary but very useful bc it disabled envlope on all channels
+  write_register(8, 0b00000000);
+  write_register(9, 0b00000000);
+  write_register(10, 0b00000000);
 }
-
-void attack(int level, int steps){ //attacks   level is how high to go max 15 steps is how many steps up each time
-for (int i = 0; i<level; i++){
-    write_register(8, 0b0000+i);
-    write_register(9, 0b0000+i);
-    write_register(10, 0b0000+i);
-    delay(steps); // if set to 0 would go up instalty 
-
-}
+int level = 15; //the max ampltiude value 
+void attack(int steps) {  //attacks   level is how high to go max 15 steps is how many steps up each time
+  for (int i = 0; i < level; i++) {
+    write_register(8, 0b0000 + i);
+    write_register(9, 0b0000 + i);
+    write_register(10, 0b0000 + i);
+    delay(steps);  // if set to 0 would go up instalty
+  }
 }
 
 
 
-void decay(int level, int steps, int attack_level){ //decay  level is how high to go max 15 steps is how many steps up each time attack is the attack level
-for (int i = attack_level; i>level; i--){
-    write_register(8, 0b0000+i);
-    write_register(9, 0b0000+i);
-    write_register(10, 0b0000+i);
-    delay(steps); // if set to 0 would go up instalty 
-
-}
-}
-
-
-void sustain(int level, int steps, int decay_level){ //sustain level is how high to go max 15 steps  (note you CAN go down!) decay level is decays level 
-
- if(decay_level > level){
-for (int i = decay_level; i>level; i--){
-    write_register(8, 0b0000+i);
-    write_register(9, 0b0000+i);
-    write_register(10, 0b0000+i);
-    delay(steps); // if set to 0 would go up instalty 
-
-}}
-else{for (int i = decay_level; i<level; i++){
-    write_register(8, 0b0000+i);
-    write_register(9, 0b0000+i);
-    write_register(10, 0b0000+i);
-    delay(steps); // if set to 0 would go up instalty 
-
-}}
+void decay(int steps, int sustain_level) {  //decay  level is how high to go max 15 steps is how many steps up each time attack is the attack level
+  for (int i = 15; i > sustain_level; i--) {
+    write_register(8, 0b0000 + i);
+    write_register(9, 0b0000 + i);
+    write_register(10, 0b0000 + i);
+    delay(steps);  // if set to 0 would go up instalty
+  }
 }
 
 
-void release(int level, int steps, int sustain_level){
-for (int i = sustain_level; i>level; i--){
-  
-    write_register(8, 0b0000+i);
-    write_register(9, 0b0000+i);
-    write_register(10, 0b0000+i);
-    delay(steps); // if set to 0 would go up instalty 
+// void sustain( ) {  // this does nothing
+// }
 
-}
+
+void release(int steps ,int sustain_level) {
+  for (int i = sustain_level; i > 0; i--) {
+
+    write_register(8, 0b0000 + i);
+    write_register(9, 0b0000 + i);
+    write_register(10, 0b0000 + i);
+    delay(steps);  // if set to 0 would go up instalty
+  }
 }
 void setup() {
-  
 
-    pinMode(14, INPUT_PULLUP);
-    pinMode(15, INPUT_PULLUP);
-    pinMode(16, INPUT_PULLUP);
-    pinMode(17, INPUT_PULLUP);
+
+  pinMode(14, INPUT_PULLUP);
+  pinMode(15, INPUT_PULLUP);
+  pinMode(16, INPUT_PULLUP);
+  pinMode(17, INPUT_PULLUP);
   // Set up Timer 1 to output a 2 MHZ clock signal in Pin 9
   TCCR1A = bit(COM1A0);
   TCCR1B = bit(WGM12) | bit(CS10);
@@ -379,7 +336,7 @@ void setup() {
   pinMode(RESET_PIN, OUTPUT);
   pinMode(BC1_PIN, OUTPUT);
   pinMode(BDIR_PIN, OUTPUT);
-  
+
 
   // Set pins 0 to 7 to output
   DDRD = 0xFF;
@@ -393,30 +350,28 @@ void setup() {
   digitalWrite(RESET_PIN, HIGH);
 
   // Enable
- 
-   write_register(7, 0b1111000);
+
+  write_register(7, 0b1111000);
   write_register(8, 0b00001111);
-
-
-
- 
 }
 
 void loop() {
-play_a(key_values[36]);
+  play_a(key_values[78]);
 
-// for(double i = 12; i < sizeof(key_values)-12; i += 2 ){
-// delay(100);
-// play_a(i);
+  // for(double i = 12; i < sizeof(key_values)-12; i += 2 ){
+  // delay(100);
+  // play_a(i);
 
 
 
-// }
-adsr_mode();
- attack(15, 20);
-decay(9, 50, 15);
-sustain(12, 30, 9);
-release(0, 200, 12);
+  // }
+  adsr_mode();
+  attack(100);
+  decay(50, 12);
+
+  release(200, 12);
+
+
 
 
 }
